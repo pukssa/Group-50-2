e36162887661d36269e87aee5b3732aa0ff09ee1
pukssa <argen@MacBook-Air--puksa.local>
2025-02-26 00:38:30 +0600

    HW-6

--- db/main_db.py
+++ db/main_db.py
@@ -1,64 +1,69 @@
-# main_db.py
 import sqlite3
 from db import queries
 
-db = sqlite3.connect('db/db.sqlite')
+# db = sqlite3.connect('db/registered.sqlite3')
+db = sqlite3.connect('db/store.sqlite3')
 cursor = db.cursor()
 
-async def create_tables():
+async def create_db():
     if db:
         print('База данных подключена')
-    cursor.execute(queries.TABLE_registered)
-    cursor.execute(queries.CREATETABLE_store)
-    cursor.execute(queries.CREATETABLE_store_detail)
+    # cursor.execute(queries.CREATE_TABLE_registered)
+    cursor.execute(queries.CREATE_TABLE_store)
+    cursor.execute(queries.CREATE_TABLE_store_detail)
 
-
-async def sql_insert_registered(fullname, age, gender, date_age, email, photo):
-    cursor.execute(queries.INSERT_TABLE_registered, (fullname, age, gender, date_age, email, photo))
+async def sql_insert_registered(fullname, age, email, city, photo):
+    cursor.execute(queries.INSERT_registered_query, (
+        fullname, age, email, city, photo
+    ))
     db.commit()
 
 
 async def sql_insert_store(name_product, size, price, photo, product_id):
-    cursor.execute(queries.INSERT_store,
-                   (name_product, size, price, photo, product_id)
-                   )
-
+    cursor.execute(queries.INSERT_store_query, (
+        name_product, size, price, photo, product_id
+    ))
     db.commit()
 
 
-async def sql_insert_store_detail(product_id, info_product, category):
-    cursor.execute(queries.INSERT_store_detail,
-                   (product_id, info_product, category))
+async def sql_insert_detail(product_id, category, info_product):
+    cursor.execute(queries.INSERT_store_detail_query, (
+        product_id, category, info_product
+    ))
     db.commit()
 
 
-    def sql_insert_collection(productid, collection):
-        cursor.execute(queries.INSERT_collection_query, (
-            productid, collection
-        ))
-    db.commit()
-# =============================================================
-
+# CRUD - 1
+# ==================================================================
 def get_db_connection():
-    conn = sqlite3.connect('db/db.sqlite')
+    conn = sqlite3.connect('db/store.sqlite3')
     conn.row_factory = sqlite3.Row
     return conn
-
 
 def fetch_all_products():
     conn = get_db_connection()
     products = conn.execute("""
-    select * from store s
-    INNER JOIN store_detail sd on s.product_id = sd.product_id
+    SELECT * from store s
+    INNER JOIN store_detail sd ON s.product_id = sd.product_id
+    INNER JOIN collection c ON s.collection_id = c.collection_id
     """).fetchall()
     conn.close()
     return products
 
-
-def delete_products(product_id):
+def delete_product(product_id):
     conn = get_db_connection()
-
+    conn.execute('DELETE FROM store WHERE product_id = ?', (product_id,))
     conn.execute('DELETE FROM store_detail WHERE product_id = ?', (product_id,))
-
     conn.commit()
     conn.close()
+
+async def get_product(product_id):
+    query = "SELECT * FROM products WHERE product_id = %s"
+    result = await db.fetch_one(query, (product_id,))
+    return result
+
+
+async def get_next_product(current_product_id):
+    query = "SELECT * FROM products WHERE product_id > %s ORDER BY product_id LIMIT 1"
+    result = await db.fetch_one(query, (current_product_id,))
+    return result
--- handlers/FSM_store.py
+++ handlers/FSM_store.py
@@ -1,11 +1,12 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 import buttons
 from db import main_db
 
-
+# Состояния для формы добавления товара
 class StoreFSM(StatesGroup):
     name_product = State()
     size = State()
@@ -17,41 +18,41 @@ class StoreFSM(StatesGroup):
     photo = State()
     submit = State()
 
+# Список товаров, для переключения по ним
+products = []
+current_product_index = 0
 
 async def start_fsm_store(message: types.Message):
     await message.answer('Введите название товара:', reply_markup=buttons.cancel)
     await StoreFSM.name_product.set()
 
-async def name_load (message: types.Message, state: FSMContext):
+async def name_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name_product'] = message.text
 
     await StoreFSM.next()
     await message.answer('Введите размер:')
 
-
-async def size_load (message: types.Message, state: FSMContext):
+async def size_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['size_product'] = message.text
 
     await StoreFSM.next()
     await message.answer('Введите цену товара:')
 
-async def price_load (message: types.Message, state: FSMContext):
+async def price_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['price_product'] = message.text
 
     await StoreFSM.next()
     await message.answer('Введите категорию товара:')
 
-
-async def category_load (message: types.Message, state: FSMContext):
+async def category_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['category_product'] = message.text
 
     await StoreFSM.next()
     await message.answer('Введите информацию о продукте:')
-
 
 async def info_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -67,7 +68,7 @@ async def collection_load(message: types.Message, state: FSMContext):
     await StoreFSM.next()
     await message.answer('Введите артикул для товара: ')
 
-async def product_id_load (message: types.Message, state: FSMContext):
+async def product_id_load(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['products_id'] = message.text
 
@@ -79,7 +80,7 @@ async def photo_load(message: types.Message, state: FSMContext):
         data['photo'] = message.photo[-1].file_id
 
     await StoreFSM.next()
-    await message.answer('Верные ли данные ?', reply_markup=buttons.submit)
+    await message.answer('Верные ли данные?', reply_markup=buttons.submit)
     await message.answer_photo(photo=data['photo'],
                            caption=f'Название товара - {data["name_product"]}\n'
                                    f'Размер товара - {data["size_product"]}\n'
@@ -92,6 +93,7 @@ async def photo_load(message: types.Message, state: FSMContext):
 async def submit_load(message: types.Message, state: FSMContext):
     if message.text == 'да':
         async with state.proxy() as data:
+            # Запись в таблицу store
             await main_db.sql_insert_store(
                 name_product=data['name_product'],
                 size=data['size_product'],
@@ -100,12 +102,14 @@ async def submit_load(message: types.Message, state: FSMContext):
                 photo=data['photo']
             )
 
+            # Запись в таблицу detail
             await main_db.sql_insert_detail(
                 product_id=data['products_id'],
                 info_product=data['info_product'],
                 category=data['category_product']
             )
 
+            # Запись в таблицу collection_products
             await main_db.sql_insert_collection(
                 productid=data['products_id'],
                 collection=data['collection']
@@ -114,7 +118,44 @@ async def submit_load(message: types.Message, state: FSMContext):
             await message.answer('Ваши данные в базе!', reply_markup=buttons.remove_keyboard)
             await state.finish()
 
+            # Добавление товара в список
+            products.append(data)  # Сохраняем в список
 
+            # Показываем первый товар
+            await show_next_product(message)
+
+
+# Создание кнопки "далее" для переключения между товарами
+async def show_next_product(message: types.Message):
+    global current_product_index
+
+    if current_product_index < len(products):
+        product = products[current_product_index]
+        current_product_index += 1
+
+        # Кнопка "далее"
+        next_button = InlineKeyboardButton(text="Далее", callback_data="next_product")
+        keyboard = InlineKeyboardMarkup().add(next_button)
+
+        await message.answer_photo(photo=product['photo'],
+                                   caption=f'Название товара - {product["name_product"]}\n'
+                                           f'Размер товара - {product["size_product"]}\n'
+                                           f'Категория - {product["category_product"]}\n'
+                                           f'Коллекция - {product["collection"]}\n'
+                                           f'Артикул - {product["products_id"]}\n'
+                                           f'Инфо - {product["info_product"]}\n'
+                                           f'Цена - {product["price_product"]}',
+                                   reply_markup=keyboard)
+    else:
+        await message.answer("Это был последний товар.", reply_markup=buttons.remove_keyboard)
+
+# Обработка нажатия на кнопку "Далее"
+async def next_product(callback_query: types.CallbackQuery):
+    global current_product_index
+    message = callback_query.message
+    await show_next_product(message)
+
+# Регистрация хендлеров
 def register_handlers_store(dp: Dispatcher):
     dp.register_message_handler(start_fsm_store, commands=['store'])
     dp.register_message_handler(name_load, state=StoreFSM.name_product)
@@ -122,7 +163,9 @@ def register_handlers_store(dp: Dispatcher):
     dp.register_message_handler(price_load, state=StoreFSM.price)
     dp.register_message_handler(category_load, state=StoreFSM.category)
     dp.register_message_handler(info_load, state=StoreFSM.info_product)
-    dp.register_message_handler(collection_load, state=StoreFSM.collection)  # Новый обработчик
+    dp.register_message_handler(collection_load, state=StoreFSM.collection)
     dp.register_message_handler(product_id_load, state=StoreFSM.products_id)
     dp.register_message_handler(photo_load, state=StoreFSM.photo, content_types=['photo'])
     dp.register_message_handler(submit_load, state=StoreFSM.submit)
+
+    dp.register_callback_query_handler(next_product, text="next_product")
